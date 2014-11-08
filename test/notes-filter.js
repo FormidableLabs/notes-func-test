e3f1b21ea11f7ab122f7f75dce6fa1f340f9378d
Ryan Roemer <ryan@loose-bits.com>
2014-11-07 18:33:57 -0800

    More test basics.

---
+++ test/notes-filter.js
@@ -0,0 +1,10 @@
+describe("notes", function () {
+
+  describe("filter", function () {
+    it("filters to no matches");
+    it("filters to one match");
+    it("filters to multiple matches");
+    it("filters to all matches");
+  });
+
+});
--- test/notes.js
+++ test/notes.js
@@ -6,62 +6,66 @@ var asserters = require("wd").asserters;
 
 describe("notes", function () {
 
-  it("adds a note and deletes it", function (done) {
-    client
-      .get(global.HOST_URL)
+  describe("items", function () {
+    it("adds a note and deletes it", function (done) {
+      client
+        .get(global.HOST_URL)
 
-      // Create a note.
-      .waitForElementByCss("input[data-qa-name='note-new-input']")
-      .type("Delete Test")
-      .waitForElementByCss("button[data-qa-name='note-new-create']")
-      .click()
-      .waitForElementByCss("[data-qa-name='note-item']:last-child",
-        asserters.textInclude("Delete Test"))
-      .text()
-      .then(function (text) {
-        expect(text).to.equal("Delete Test");
-      })
+        // Create a note.
+        .waitForElementByCss("input[data-qa-name='note-new-input']")
+        .type("Delete Test")
+        .waitForElementByCss("button[data-qa-name='note-new-create']")
+        .click()
+        .waitForElementByCss("[data-qa-name='note-item']:last-child",
+          asserters.textInclude("Delete Test"))
+        .text()
+        .then(function (text) {
+          expect(text).to.equal("Delete Test");
+        })
 
-      // // Delete a note
-      // // TODO REMOVE?
-      // .waitForElementByCss(
-      //   "[data-qa-name='note-item']:last-child " +
-      //   "[data-qa-name='note-item-delete']")
-      // .click()
+        // // Delete a note
+        // // TODO REMOVE?
+        // .waitForElementByCss(
+        //   "[data-qa-name='note-item']:last-child " +
+        //   "[data-qa-name='note-item-delete']")
+        // .click()
 
-      .nodeify(done);
+        .nodeify(done);
+    });
+
+    it("adds a note and edits it", function (done) {
+      client
+        .get(global.HOST_URL)
+
+        // Create a note.
+        .waitForElementByCss("input[data-qa-name='note-new-input']")
+        .type("Edit Test")
+        .waitForElementByCss("button[data-qa-name='note-new-create']")
+        .click()
+        .waitForElementByCss("[data-qa-name='note-item']:last-child",
+          asserters.textInclude("Edit Test"))
+        .text()
+        .then(function (text) {
+          expect(text).to.equal("Edit Test");
+        })
+
+        // Edit the note.
+        // .waitForElementByCss(".notes-item .note-edit")
+        // .click()
+        // .url()
+        // .then(function (url) {
+        //   expect(url).to.match(/\/note\/.*\/edit$/);
+        // })
+        // .waitForElementByCss("#input-title")
+        // .getValue()
+        // .then(function (val) {
+        //   expect(val).to.equal("Edit Test");
+        // })
+
+        .nodeify(done);
+    });
+
+    it("adds a note and views it");
   });
 
-  it("adds a note and edits it", function (done) {
-    client
-      .get(global.HOST_URL)
-
-      // Create a note.
-      .waitForElementByCss("input[data-qa-name='note-new-input']")
-      .type("Edit Test")
-      .waitForElementByCss("button[data-qa-name='note-new-create']")
-      .click()
-      .waitForElementByCss("[data-qa-name='note-item']:last-child",
-        asserters.textInclude("Edit Test"))
-      .text()
-      .then(function (text) {
-        expect(text).to.equal("Edit Test");
-      })
-
-      // Edit the note.
-      // .waitForElementByCss(".notes-item .note-edit")
-      // .click()
-      // .url()
-      // .then(function (url) {
-      //   expect(url).to.match(/\/note\/.*\/edit$/);
-      // })
-      // .waitForElementByCss("#input-title")
-      // .getValue()
-      // .then(function (val) {
-      //   expect(val).to.equal("Edit Test");
-      // })
-
-      .nodeify(done);
-  });
 });
-
